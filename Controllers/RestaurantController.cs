4997d80a2192fe7083036da1d1398b46ba63a50f
Saowapaa <51878605+Saowapaa@users.noreply.github.com>
2019-07-01 16:14:47 +0700

    Add files via upload

---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIRESTful.Models;
+
+namespace WebAPIRESTful.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+
+        private Restaurant[] Restaurants = new Restaurant[]
+        {
+            //บะหมี่ปราบเซียน+ประชาชื่น+8/@13.8069079,100.541996
+            new Restaurant{Id = 1, Name = "บะหมี่ปราบเซียน ประชาชื่น 8",Description="293 27 ซอย ประชาชื่น 8 แขวง บางซื่อ เขตบางซื่อ กรุงเทพมหานคร 10800",Lat =13.8069079,Lng=100.541996},
+            new Restaurant{Id = 2, Name = "ก๋วยเตี๋ยวเตาปูน(ล็อคล้อ)",Description="10, 6 ถนน ประชาชื่น แขวง บางซื่อ เขตบางซื่อ กรุงเทพมหานคร 10800",Lat =13.8142846,Lng=100.5432543},
+            new Restaurant{Id = 3, Name = "อร่อยแล้วสั่งต่อ",Description="812/10 ซอย ประชาชื่น 25 ถนน ประชาชื่น แขวง วงศ์สว่าง เขตบางซื่อ กรุงเทพมหานคร 10800",Lat =13.8149514,Lng=100.5259594},
+            new Restaurant{ Id = 4, Name = "คำโฮม คาเฟ่",Description="คอนโดยูดีไลท์ 695/4 ซอย ประชาชื่น 19 แขวง บางซื่อ เขตบางซื่อ กรุงเทพมหานคร 10800",Lat =13.8184855,Lng=100.5359784}
+        };
+
+        // GET: api/Restaurant
+        public IEnumerable<Restaurant> Get()
+        {
+            return Restaurants;
+        }
+
+        // GET: api/Restaurant/5
+        public Restaurant Get(int id)
+        {
+            Restaurant Restaurant = Restaurants.Where(e => e.Id == id).First();
+            return Restaurant;
+        }
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST: api/Restaurant
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Restaurant/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Restaurant/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Web/scripts/main.js
@@ -0,0 +1,112 @@
+﻿var position = { lat: 13.8170403, lng: 100.5200337 };//บางซื่อ
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            Restaurants: null
+        }
+    },
+    methods: {
+        onLoad: async function (data) {
+            console.log("OnLoad()");
+
+            var maps = new google.maps.Map(document.getElementById('map'), {
+                center: position,
+                zoom: 14
+            });
+
+            for (var i = 0; i < data.length; i++) {
+
+                marker = new google.maps.Marker({
+                    animation: google.maps.Animation.DROP,
+                    position: new google.maps.LatLng(data[i].Lat, data[i].Lng),
+                    title: data[i].Name,
+                    map: maps,
+
+                });
+                google.maps.event.addListener(marker, 'click', (function (marker, i) {
+                    return function () {
+
+                        if (marker.getAnimation() !== null) {
+                            marker.setAnimation(null);
+                        } else {
+                            marker.setAnimation(google.maps.Animation.BOUNCE)
+                        }
+                    }
+
+                })(marker, i));
+
+            }
+
+        },
+        onClickRestaurant: async function (Restaurant) {
+
+            var maps = new google.maps.Map(document.getElementById('map'), {
+                center: { lat: Restaurant.Lat, lng: Restaurant.Lng },
+                zoom: 14
+            });
+
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(Restaurant.Lat, Restaurant.Lng),
+                title: Restaurant.Name,
+                map: maps,
+            });
+
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+
+        },
+        onSearch: async function () {
+
+            var input = this.txt_search;
+            var resultsMap = new google.maps.Map(document.getElementById('map'), {
+                zoom: 14,
+                center: position,
+            });
+            var geocoder = new google.maps.Geocoder();
+
+            geocoder.geocode({ 'address': input }, function (results, status) {
+                if (status === 'OK') {
+                    resultsMap.setCenter(results[0].geometry.location);
+                    new google.maps.Marker({
+                        map: resultsMap,
+                        position: results[0].geometry.location
+                    });
+                } else {
+                    alert('Geocode was not succressful for the following reason : ' + status);
+                }
+
+
+            });
+
+        }
+    },
+    mounted() {
+        fetch("http://localhost:53157/api/Restaurant")//connect Web api rest ful
+            .then(response => response.json())
+            .then((data) => {
+
+                this.Restaurants = data;
+                this.onLoad(data);
+            })
+
+    }
+    //template: `
+    //<table class="table table-hover">
+    //    <tr>
+    //        <th style="width: 20px">#</th>
+    //        <th>ชื่อ</th>
+    //        <th>ที่อยู่</th>
+    //        <th style="width: 40px">Label</th>
+    //    </tr>
+    //    <tr v-for="Restaurant, i in Restaurants">
+    //        <td>{{Restaurant.Id}}.</td>
+    //        <td>{{Restaurant.Name}}</td>
+    //        <td>{{Restaurant.Description}}</td>
+    //        <td></td>
+    //    </tr>
+    //</table>
+
+    //`,
+
+});
+function initMap() { }
